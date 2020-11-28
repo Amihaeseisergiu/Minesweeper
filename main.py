1e10101d4365c0eca38917651d72d0e289674b3d
Amihaeseisergiu <61450080+Amihaeseisergiu@users.noreply.github.com>
2020-11-28 23:53:30 +0200

    Game Over Screen and Countdown
    - added game over screen with explicit text and a button to return to the main menu
    - added a countdown timer that redirects to the game over screen when the time ran down
    - added more sprites

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
+import time
+import threading
+
+time_limit = 0
 
 class Entry_number(Entry):
     def __init__(self, master=None, **kwargs):
@@ -35,10 +39,10 @@ game_name_label = Label(game_name_frame, text = "Minesweeper")
 game_name_label.grid(row = 0, padx = 20, pady = 10, column = 0)
 game_name_label.config(font=("Courier", 30))
 
-game_image = PhotoImage(file = "./textures/menu_face.png")
-game_image = game_image.zoom(20) #with 250, I ended up running out of memory
-game_image = game_image.subsample(32)
-menu_image = Label(game_name_frame, image = game_image)
+image_face_happy = PhotoImage(file = "./textures/face_happy.png")
+image_face_happy = image_face_happy.zoom(20)
+image_face_happy = image_face_happy.subsample(32)
+menu_image = Label(game_name_frame, image = image_face_happy)
 menu_image.grid(row = 1, column = 0)
 
 game_options_frame = Frame(main_menu_frame)
@@ -86,6 +90,30 @@ def mainMenu(itself):
     itself.grid_forget()
     main_menu_frame.grid(row = 0, column = 0, sticky = 'news')
 
+def gameOver(itself, reason):
+    itself.grid_forget()
+    final_screen_frame = Frame(root)
+    final_screen_frame.grid(row = 0, column = 0, sticky = 'we')
+    final_screen_frame.rowconfigure(0, weight = 1)
+    final_screen_frame.columnconfigure(0, weight = 1)
+
+    final_screen_label = Label(final_screen_frame, font = "Courier 20")
+    image_face_dead = PhotoImage(file = "./textures/face_dead.png")
+    image_face_dead = image_face_dead.zoom(10)
+    image_face_dead = image_face_dead.subsample(32)
+    final_screen_image = Label(final_screen_frame, image = image_face_dead)
+
+    if(reason == 'time'):
+        final_screen_label['text'] = "You lost! Time ran down"
+        final_screen_image.image = image_face_dead
+    
+    final_screen_label.grid(row = 0, column = 0)
+    final_screen_image.grid(row = 1, column = 0)
+
+    main_menu_button = Button(final_screen_frame, font = "Arial 15", text = "Main Menu")
+    main_menu_button['command'] = lambda itself = final_screen_frame: mainMenu(itself)
+    main_menu_button.grid(row = 2, column = 0, pady = 10)
+
 def startGame():
     if table_size_entry_height.get() == "" and table_size_entry_width.get() != "":
         messagebox.showerror("Error", "Height has to be filled")
@@ -97,9 +125,37 @@ def startGame():
         messagebox.showerror("Error", "Width has to be positive")
     else:
         main_menu_frame.grid_forget()
-        placeholder = Button(root, text = "Main Menu")
-        placeholder['command'] = lambda itself = placeholder: mainMenu(itself)
-        placeholder.grid(row = 0, column = 0)
+
+        nr_bombs = 10
+        table_height = 10
+        table_width = 10
+        global time_limit
+        time_limit = 60
+        if table_size_entry_height.get() != "" and table_size_entry_width.get() != "":
+            table_height = int(table_size_entry_height.get())
+            table_width = int(table_size_entry_width.get())
+        if nr_bombs_entry.get() != "":
+            nr_bombs = int(nr_bombs_entry.get())
+        if time_limit_entry.get() != "":
+            time_limit = int(time_limit_entry.get())
+
+        main_game_frame = Frame(root)
+        main_game_frame.grid(row = 0, column = 0, sticky = 'news')
+
+        time_limit_label = Label(main_game_frame, text = '{:02}:{:02}'.format(time_limit%3600//60, time_limit%60))
+        time_limit_label.grid(row = 0, column = 0)
+
+        def countdown():
+            global time_limit
+            while time_limit > 0:
+                time.sleep(1)
+                time_limit -= 1
+                time_limit_label['text'] = '{:02}:{:02}'.format(time_limit%3600//60, time_limit%60)
+            gameOver(main_game_frame, "time")
+
+        countdown_thread = threading.Thread(target=countdown)
+        countdown_thread.daemon = True
+        countdown_thread.start()
 
 start_game_button = Button(start_game_frame, text = "Start", font = "Arial 15", command = startGame)
 start_game_button.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = "we") 
